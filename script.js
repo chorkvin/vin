b2bca12655196e1c3d6933fc515d2d5ed6b47e66
chorkvin <145746951+chorkvin@users.noreply.github.com>
2023-09-22 13:32:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+let menu = document.querydelector('#menu-bars');
+let navbar = document.querydelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navber.classList.toggle('active');
+}
+Window.onsroll = () =>{
+    menu.classList.remove('fa-times');
+    navber.classList.remove('active');
+}
+document.querySelector('#search-icon').onclick = () =>{
+document.querySelector('#search-form') .classList.toggle('active')  
+}
+
+document.querySelector('#close').onclick = () =>{
+document.querySelector('#search-form') .classList.remove('active') 
+} 
+
+var swiper = new Swiper("home-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
+
+  var swiper = new Swiper("review-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    loop:true,
+    breakpoints:{
+      0: {
+        slidesPerView: 1,
+      },
+      640:{
+        slidesPerView: 2,
+      },
+      768:{
+        slidesPerView: 2,
+      },
+      1024:{
+        slidesPerView: 3,
+      },
+    },
+  });
